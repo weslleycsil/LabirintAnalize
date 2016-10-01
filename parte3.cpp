aad451a8d16294db75cb177842ce685ab8696a3f
Weslley Silva <weslleycsil@gmail.com>
2016-10-01 16:26:42 -0300

    Finalização 0.9b
    Funciona, porem quando há apenas um caminho.

--- parte3.cpp
+++ parte3.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 int **matriz;
+int **matrizAux; 
+
 int colunas,linhas;
 
 int *achaEntrada(int x, int y){
@@ -156,46 +158,60 @@ class Arvore{
         }
 
         // InserÃ§Ã£o
-        void insere(int l, int c, int newValor) {
-            if(raiz == NULL) {// Arvore esta vazia?
+        void insere(int l, int c, int newValor, Nodo *raizAtual) {
+            if(raizAtual == NULL) {// Arvore esta vazia?
                 raiz = new Nodo(l, c, newValor);// Sim, Cria raiz.
             } else {
-               insereAux(raiz, l, c, newValor); // NÃ£o, InserÃ§Ã£o recurssiva.
+               insereAux(raizAtual, l, c, newValor); // NÃ£o, InserÃ§Ã£o recurssiva.
             }
         }
 
         void insereAux(Nodo *raiz, int l, int c, int newValor) {// Recurssivo.
-            if(l < raiz->getL()){
-                //insere a esquerda
-                if(raiz->getEsq() == NULL) {// Raiz nÃ£o tem filho a esquerda?
-                    Nodo *novoNodo = new Nodo(l,c, newValor);// Cria um novo Nodo e o seta na esquerda.
-                    novoNodo->setPai(raiz);
-                    raiz->setEsq(novoNodo);
-                    raiz->getEsq()->setPos('E');
+            //cout << "Inserindo ou tentando: L-> " << l << " C-> " << c << endl;
+            //cout << "Consulta: " << (consulta(l, c) != 1) << endl;
+            if(consulta(l, c) != 1){
+                if(l < raiz->getL()){
+                    //insere a esquerda
+                    if(raiz->getEsq() == NULL) {// Raiz nÃ£o tem filho a esquerda?
+                        Nodo *novoNodo = new Nodo(l,c, newValor);// Cria um novo Nodo e o seta na esquerda.
+                        novoNodo->setPai(raiz);
+                        raiz->setEsq(novoNodo);
+                        raiz->getEsq()->setPos('E');
+                        /*raiz->mostra();
+                        raiz->getEsq()->mostra();
+                        cout << endl << endl;*/
+                    } else {
+                        //ja tem filho a esquerda
+                        insereAux(raiz->getEsq(), l,c, newValor);// Pega o Nodo a esquerda e a chave como parametro.
+                    }
+                } else if (l > raiz->getL()){
+                    //insere a direita
+                    if(raiz ->getDir() == NULL) {// Raiz nÃ£o tem filho a direita?
+                        Nodo *novoNodo = new Nodo(l,c, newValor);// Cria um novo Nodo e o seta na direita.
+                        novoNodo->setPai(raiz);
+                        raiz->setDir(novoNodo);
+                        raiz->getDir()->setPos('D');
+                        /*raiz->mostra();
+                        raiz->getDir()->mostra();
+                        cout << endl << endl;*/
+                    } else {
+                        insereAux(raiz->getDir(), l,c, newValor);// Pega o Nodo a direita e a chave como parametro.
+                    }
                 } else {
-                    //ja tem filho a esquerda
-                    insereAux(raiz->getEsq(), l,c, newValor);// Pega o Nodo a esquerda e a chave como parametro.
+                    //insere no meio
+                    if(raiz ->getMeio() == NULL) {// Raiz nÃ£o tem filho no meio?
+                        Nodo *novoNodo = new Nodo(l,c, newValor);// Cria um novo Nodo e o seta na direita.
+                        novoNodo->setPai(raiz);
+                        raiz->setMeio(novoNodo);
+                        raiz->getMeio()->setPos('M');
+                        /*raiz->mostra();
+                        raiz->getMeio()->mostra();
+                        cout << endl << endl;*/
+                    } else {
+                        insereAux(raiz->getMeio(), l,c, newValor);// Pega o Nodo a direita e a chave como parametro.
+                    }
                 }
-            } else if (l > raiz->getL()){
-                //insere a direita
-                if(raiz ->getDir() == NULL) {// Raiz nÃ£o tem filho a direita?
-                    Nodo *novoNodo = new Nodo(l,c, newValor);// Cria um novo Nodo e o seta na direita.
-                    novoNodo->setPai(raiz);
-                    raiz->setDir(novoNodo);
-                    raiz->getDir()->setPos('D');
-                } else {
-                    insereAux(raiz->getDir(), l,c, newValor);// Pega o Nodo a direita e a chave como parametro.
-                }
-            } else {
-                //insere no meio
-                if(raiz ->getMeio() == NULL) {// Raiz nÃ£o tem filho no meio?
-                    Nodo *novoNodo = new Nodo(l,c, newValor);// Cria um novo Nodo e o seta na direita.
-                    novoNodo->setPai(raiz);
-                    raiz->setMeio(novoNodo);
-                    raiz->getMeio()->setPos('M');
-                } else {
-                    insereAux(raiz->getMeio(), l,c, newValor);// Pega o Nodo a direita e a chave como parametro.
-                }
+
             }
         }
 
@@ -206,17 +222,23 @@ class Arvore{
         void mostra(Nodo *raiz) {//PrÃ©-fixada.
             if(raiz != NULL) {
                 raiz->mostra();
+                
                 mostra(raiz->getEsq());
                 mostra(raiz->getMeio());
                 mostra(raiz->getDir());
             }
         }
 
+        int consulta(int l, int c){
+            return consultaNodo(l, c, getRaiz());
+
+        }
+        
         int consultaNodo(int l, int c, Nodo *raiz){
             int val = 0;
             if(raiz != NULL) {
-                raiz->mostra();
-                cout << ((raiz->getL() == l) && (raiz->getC() == c)) << endl;
+                //raiz->mostra();
+                //cout << ((raiz->getL() == l) && (raiz->getC() == c)) << endl;
                 if((raiz->getL() == l) && (raiz->getC() == c)){
                     return 1;
                 }
@@ -229,52 +251,89 @@ class Arvore{
         }
 
         void preencheArvore(Nodo *raiz){
-            
+            cout << "Raiz: " << (raiz!=NULL) <<endl;
             if(raiz != NULL) {
                 cout << "Recursividade" << endl;
                 int linhaAtual = raiz->getL();
-            int colunaAtual = raiz->getC();
-
-            if(linhaAtual-1 < linhas){
-                //verifico se o valor da linha é diferente de 0
-                cout << "Filho esquerda" << endl;
-                if(matriz[linhaAtual-1][colunaAtual] != 0){
-                    if(1){
-
+                int colunaAtual = raiz->getC();
+                cout << "Linha-> " << linhaAtual << " Coluna-> " << colunaAtual << endl;
+                if((linhaAtual-1 < linhas) && (linhaAtual-1 >= 0)){// erro da posição 0
+                    //verifico se o valor da linha é diferente de 0
+                    cout << "Filho esquerda" << endl;
+                    if(matriz[linhaAtual-1][colunaAtual] != 0){
+                        cout << "tento inserir filho esquerda" << endl;
+                        insere(linhaAtual-1, colunaAtual, matriz[linhaAtual-1][colunaAtual],raiz);
+                        //raiz->getEsq()->setPos('E');
                     }
-                    cout << "tento inserir filho esquerda" << endl;
-                    insere(linhaAtual-1, colunaAtual, matriz[linhaAtual-1][colunaAtual]);
-                    //raiz->getEsq()->setPos('E');
-                }
-            }//esquerda
+                }//esquerda
 
-            if(colunaAtual+1 < colunas){
-                //verifico se o valor da linha é diferente de 0
-                cout << "filho do meio" << endl;
-                if(matriz[linhaAtual][colunaAtual+1] != 0){
-                    cout << "tento inserir filho no meio" << endl;
-                    insere(linhaAtual, colunaAtual+1, matriz[linhaAtual][colunaAtual+1]);
-                    //raiz->getMeio()->setPos('M');
-                }
-            }//meio
+                if(colunaAtual+1 < colunas){
+                    //verifico se o valor da linha é diferente de 0
+                    //cout << "filho do meio" << endl;
+                    if(matriz[linhaAtual][colunaAtual+1] != 0){
+                        //cout << "tento inserir filho no meio" << endl;
+                        insere(linhaAtual, colunaAtual+1, matriz[linhaAtual][colunaAtual+1],raiz);
+                        //raiz->getMeio()->setPos('M');
+                    }
+                }//meio
 
-            if(linhaAtual+1 < linhas){
-                //verifico se o valor da linha é diferente de 0
-                cout << "Filho da direita" << endl;
-                if(matriz[linhaAtual+1][colunaAtual] != 0){
-                    cout << "tento inserir filho direita" << endl;
-                    insere(linhaAtual+1, colunaAtual, matriz[linhaAtual+1][colunaAtual]);
-                    //raiz->getDir()->setPos('D');
-                }
-            }//direito
-            //fazer comparacao se ja existe
+                if(linhaAtual+1 < linhas){
+                    //verifico se o valor da linha é diferente de 0
+                    //cout << "Filho da direita" << endl;
+                    if(matriz[linhaAtual+1][colunaAtual] != 0){
+                        //cout << "tento inserir filho direita" << endl;
+                        insere(linhaAtual+1, colunaAtual, matriz[linhaAtual+1][colunaAtual],raiz);
+                        //raiz->getDir()->setPos('D');
+                    }
+                }//direito
+                //fazer comparacao se ja existe
+                //cout << "Chamo da esquerda" << endl;
                 preencheArvore(raiz->getEsq());
+                //cout << "Chamo do Meio" << endl;
                 preencheArvore(raiz->getMeio());
+                //cout << "Chamo da Direta" << endl;
                 preencheArvore(raiz->getDir());
             }
         }
 
+        Nodo *busca(int valor, Nodo **raiz) {
+            if(*raiz != NULL) {
+                if(valor == (*raiz)->getValor()) {
+                    cout << "Achei" <<endl;
+                    return * raiz;
+                }
+
+                if((*raiz)->dir != NULL){
+                    return busca(valor, &(*raiz)->dir);
+                }
+                if((*raiz)->meio != NULL){
+                    return busca(valor, &(*raiz)->meio);
+                }
+                if((*raiz)->esq != NULL){
+                    return busca(valor, &(*raiz)->esq);
+                }
+            }
+        }
+
+        void pais(Nodo *saida){
+            if(saida != NULL) {
+                saida->mostra();
+                pais(saida->getPai());
+            }
+        }
+
+        void paisRecursivos(){
+            int saida = 200;
+            Nodo *noBusca;
+            noBusca = busca(saida, &raiz); // retorno o nodo saida do lab
+            pais(noBusca);
+
+        }
+
+
 };
+
+
 
 int main() {
     
@@ -302,6 +361,12 @@ int main() {
     for (int i = 0; i < linhas; i++) {
         //colunas
         matriz[i] = new int[colunas];
+    }
+    //alocação da Matriz auxilia
+    matrizAux = new int*[linhas];
+    for (int i = 0; i < linhas; i++) {
+        //colunas
+        matrizAux[i] = new int[colunas];
     }
 
     //percorro inserindo valores na matriz
@@ -335,12 +400,13 @@ int main() {
     
     
     Arvore test;
+    Nodo * teste;
    
-    test.insere(inicio[0],inicio[1],matriz[inicio[0]][inicio[1]]);
+    test.insere(inicio[0],inicio[1],matriz[inicio[0]][inicio[1]],test.getRaiz());
 
     test.preencheArvore(test.getRaiz());
 
-    cout << "Valor: " << test.consultaNodo(1,2,test.getRaiz()) << endl;
+    //cout << "Valor: " << test.consultaNodo(1,2,test.getRaiz()) << endl;
     
     //test.consulta(13);
       
@@ -354,6 +420,11 @@ int main() {
     // Mostra em ordem.
     cout << "\n--Percorrendo em ordem...--\n";
     test.mostra(test.getRaiz());
+    cout << "\n ---------\n"<< endl;
+    //teste = test.busca(200,test.getRaiz());
+    //teste->mostra();
+    //test.consulta(10);
+    test.paisRecursivos();
     /*
     // Mostra invertido.
     cout << "\nPercorrendo pos..\n";
